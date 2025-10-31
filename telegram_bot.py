973dfdb425c1d610f3ce804efadd73d1bd66f3b2
Zney4 <134953901+Zney4@users.noreply.github.com>
2025-10-31 19:45:06 +0300

    fix

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,44 +2,46 @@ import os
 import telegram
 from dotenv import load_dotenv
 import asyncio
-from random import randint
-import random
+from random import randint, choice
 import requests
 
 
-def get_random_comics(random_number):
-    url = f"https://xkcd.com/{random_number}/info.0.json"
+def get_comics_by_number(number_comics):
+    url = f"https://xkcd.com/{number_comics}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
 
-def downloading_random_image(random_number, random_photo, directory_image):
-    response = requests.get(random_photo["img"])
+def download_image(comics_number, comics_image, file_path):
+    response = requests.get(comics_image["img"])
     response.raise_for_status()
-    filename_random = f"comics{random_number}.png"
-    with open(os.path.join(directory_image, filename_random), "wb") as file:
+    file_name = f"comic_{comics_number}.png"
+    with open(os.path.join(file_path, file_name), "wb") as file:
         file.write(response.content)
 
 
-async def public_comics(chat_id, random_file, random_photo):
+async def publishing_comics(chat_id, choice_comics, comics_image):
     await bot.send_document(
-        chat_id=chat_id, document=open(os.path.join(directory_image, random_file), "rb")
+        chat_id=chat_id, document=open(os.path.join(file_path, choice_comics), "rb")
     )
-    await bot.send_message(chat_id, f'{random_photo["alt"]}')
+    await bot.send_message(chat_id, f'{comics_image["alt"]}')
+
 
 if __name__ == "__main__":
     load_dotenv()
-    if not os.path.exists("image"):
-        os.makedirs("image")
-    random_number = randint(1, 3150)
-    random_photo = get_random_comics(random_number)
-    directory_image = "image"
-    downloading_random_image(random_number, random_photo, directory_image)
-
-    directory = os.listdir("image/")
     bot = telegram.Bot(token=os.environ["TG_TOKEN"])
     chat_id = os.environ["CHAT_ID"]
-    random_file = random.choice(directory)
-    asyncio.run(public_comics(chat_id, random_file, random_photo))
-    os.remove(os.path.join(directory_image, random_file))
+
+    file_path = "images"
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    try:
+        comics_number = randint(1, 3150)
+        comics_image = get_comics_by_number(comics_number)
+        download_image(comics_number, comics_image, file_path)
+        selection_comics = os.listdir(file_path)
+        choice_comics = choice(selection_comics)
+        asyncio.run(publishing_comics(chat_id, choice_comics, comics_image))
+    finally:
+        os.remove(os.path.join(file_path, choice_comics))
